712ddfbda49149a9b968ed6c3b7322d876561a47
null-architect <stry.bout.eyes@gmail.com>
2021-04-10 23:03:30 +0900

    Add term check

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ export default function App() {
 			const data = await res.json()
 			setImages(data.hits)
 			setIsLoading(false)
-			console.log(data)
 		}
-		fetchImages()
+		if (term) {
+			fetchImages()
+		}
 	}, [term])
 	
 	return (
