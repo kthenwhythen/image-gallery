6af73ff3b07e366e31b65afb2520d171cf52174a
null-architect <stry.bout.eyes@gmail.com>
2021-04-12 11:38:46 +0900

    Change bg color in Index, Add style to header, add if states for styles

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -9,11 +9,9 @@ export default function SearchForm({ searchText }) {
 	}
 
     return (
-        <form onSubmit={formHandler} className="w-full max-w-sm">
-            <input onChange={e => setText(e.target.value)} placeholder="Type something..." className="h-8 font-chakra text-center rounded block bg-gray-100 w-full focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text"/>
-            <div className="flex justify-center py-3">
-                <button className="w-24 h-8 rounded font-chakra bg-gray-900 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50" type="submit">Search</button>
-            </div>
+        <form onSubmit={formHandler} className="w-full max-w-sm flex justify-center">
+            <input onChange={e => setText(e.target.value)} placeholder="Type something..." className="h-8 font-chakra text-center rounded block bg-gray-100 w-full mr-3 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text"/>
+            <button className="w-24 h-8 rounded font-chakra bg-gray-900 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50" type="submit">Search</button>
         </form> 
     )
 }
